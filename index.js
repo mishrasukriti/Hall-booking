bf1b435963d477572ae5dc75bb8c8b72cc300264
Sukriti Mishra <67943627+mishrasukriti@users.noreply.github.com>
2020-11-18 22:41:22 +0530

    Update index.js
    added some sample changes

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ let rooms = [];
 let bookings = new Map();
 
 let customer = [];
-
+app.post("/hello", (req,res)=>{
+    console.log("req.body", req.body);
+    res.status(200).json({
+        message: "Heloo world",
+        
+    });
+});
 app.post("/createRoom", (req,res)=>{
     console.log("req.body", req.body);
     
